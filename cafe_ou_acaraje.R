49e13ff56872a0ceef82edbe4a745f1b4bc34849
GuilhermeOliGou <guilherme.oligou@gmail.com>
2021-08-19 21:13:16 -0300

    Versão final

--- cafe_ou_acaraje.R
+++ cafe_ou_acaraje.R
@@ -1,32 +1,22 @@
-markov.main <- function(iteracoes_extras=100, m=150){
-  transicoes = read.table("dado.txt")
+markov.main <- function(posicao, m=50){
+  transicoes = as.matrix(read.table("dado.txt"))
   transicoes = transicoes*(1/6)
-  transicoes - t(transicoes)
+  transicoes = t(transicoes)
   
-  inicial = c(1,0,0,0,0,0,0)
+  inicial = as.matrix(c(1,0,0,0,0,0,0))
   
   p.inferior = inicial
   p.superior = transicoes %*% inicial
-  pr.inferior = p.inferior[6]
-  pr.superior = p.inferior[6] + 1 - p.superior[7]
+  pr.inferior = p.inferior[posicao]
+  pr.superior = p.inferior[posicao] + 1 - p.superior[7]
   pr.final = (pr.inferior + pr.superior)/2
   
   for (i in 2:m) {
     
     p.inferior = p.superior
     p.superior = transicoes %*% p.inferior
-    pr.inferior = p.inferior[6]
-    pr.superior = p.inferior[6] + 1 - p.superior[7]
-    pr.final = (pr.inferior + pr.superior)/2
-    
-  }
-  
-  for (i in 1:iteracoes_extras) {
-    
-    p.inferior = p.superior
-    p.superior = transicoes %*% p.inferior
-    pr.inferior = p.inferior[6]
-    pr.superior = p.inferior[6] + 1 - p.superior[7]
+    pr.inferior = pr.inferior + (p.inferior[posicao])
+    pr.superior = pr.inferior + 1 - p.superior[7]
     pr.final = (pr.inferior + pr.superior)/2
     
     print(pr.final)
